91b8e21638272ca0375def9fd5c9145083a1a646
Bendyk-Vitalii <75281160+Bendyk-Vitalii@users.noreply.github.com>
2021-11-10 13:47:52 +0200

    +memo

--- src/App.js
+++ src/App.js
@@ -1,63 +1,93 @@
-import React, { useEffect, useState } from 'react';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import { GlobalStyle } from './globalStyle';
-import Hero from './components/Hero';
-import Products from './components/Products';
-import Feature from './components/Feature';
-import Footer from './components/Footer';
-import Cart from './components/Navbar/CartModal'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { GlobalStyle } from "./globalStyle";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import Feature from "./components/Feature";
+import Footer from "./components/Footer";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { commerce } from '../src/lib/commerce'
+import { commerce } from "./lib/commerce";
+import { Element } from "react-scroll";
 
+export const CartContext = React.createContext([]);
 function App() {
-  //All product
-  const [products, setProducts] = useState([])
-  //add product cart to state
-  const [cart, setCart] = useState({})
-  
+  const [products, setProducts] = useState([]);
+
+  const [cart, setCart] = useState({});
+
   const fetchProducts = async () => {
-    const {data} = await commerce.products.list()
-    setProducts(data)
-  }
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
 
   const fetchCart = async () => {
-    setCart(await commerce.cart.retrieve())
-  }
+    setCart(await commerce.cart.retrieve());
+  };
   //add cart to server
-  const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity)
-    setCart(item.cart)
-  }
+  const handleAddToCart = async (productId) => {
+    const item = await commerce.cart.add(productId);
+    setCart(item.cart);
+  };
+
+  const updateCartQty = async (productId, quantity) => {
+    const cart = await commerce.cart.add(productId);
+    setCart(cart);
+  };
+
+  const RemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
+  };
 
   useEffect(() => {
-    fetchProducts()
-    fetchCart()
-    Aos.init({duration: 1000 })
-  }, [])
+    fetchProducts();
+    fetchCart();
+    Aos.init({ duration: 1000 });
+  }, []);
 
+  const pizza = products.filter((el) => el.sku === "pizza");
+  const sweets = products.filter((el) => el.sku === "sweets");
+  const productOfTheDay = products.filter(
+    (el) => el.sku === "Pizza of the Day"
+  );
+  const MemoizedHeroComponent = React.memo(Hero)
+  const MemoizedFooterComponent = React.memo(Footer);
   return (
     <Router>
       <GlobalStyle />
-      <Hero cart={cart} />
-        <Products 
-      heading='Choose your favorite' 
-      data={products.filter((el) => el.sku === 'pizza')}
-      buttonAddHandler={handleAddToCart}
-      />
-
-      <Feature 
-      data={products.filter((el) => el.sku === 'Pizza of the Day')}
-      buttonAddHandler={handleAddToCart}
-      />
-
-      <Products 
-      heading='Choose sweets' 
-      data={products.filter((el) => el.sku === 'sweets')}
-      buttonAddHandler={handleAddToCart}
-      />
-
-      <Footer />
+      <CartContext.Provider
+        value={{
+          updateCartQty,
+          RemoveFromCart,
+          handleEmptyCart,
+          setCart,
+          cart,
+        }}
+      >
+        <MemoizedHeroComponent />
+      </CartContext.Provider>
+      <Element name="pizza">
+        <Products
+          heading="Choose your favorite"
+          data={pizza}
+          buttonAddHandler={handleAddToCart}
+        />
+      </Element>
+      <Feature data={productOfTheDay} buttonAddHandler={handleAddToCart} />
+      <Element name="desserts">
+        <Products
+          heading="Choose sweets"
+          data={sweets}
+          buttonAddHandler={handleAddToCart}
+        />
+      </Element>
+      <MemoizedFooterComponent />
     </Router>
   );
 }
