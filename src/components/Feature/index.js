69623e6c18f7046ba5db6b6b4867cea926284864
Bendyk-Vitalii <75281160+Bendyk-Vitalii@users.noreply.github.com>
2021-11-19 18:57:33 +0200

    Add files via upload

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { FeatureContainer, FeatureButton } from "./Feature";
+import { useDispatch } from 'react-redux';
+import { AddToCart } from '../../Redux/cart';
 
-const Feature = ({ data, buttonAddHandler }) => {
-
+const Feature = ({ data }) => {
+  const dispatch = useDispatch()
   return (
     <FeatureContainer>
       <h1>Pizza of the Day</h1>
       {data.map((product) => {
-
+        
         return (
           <>
             <p dangerouslySetInnerHTML={{ __html: product.description }}></p>
-            <FeatureButton onClick={() => buttonAddHandler(product.id)}>
+            <FeatureButton onClick={() => dispatch(AddToCart(product.id))}>
               Order Now
-            </FeatureButton>
+            </FeatureButton> 
           </>
         );
       })}
