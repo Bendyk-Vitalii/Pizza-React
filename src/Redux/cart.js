ff892ac9634c40cdabb9368aaed6752b3a291060
Bendyk-Vitalii <75281160+Bendyk-Vitalii@users.noreply.github.com>
2021-11-22 14:46:23 +0200

    +/-

--- src/Redux/cart.js
+++ src/Redux/cart.js
@@ -56,7 +56,7 @@ export const EmptyCartHandler = createAsyncThunk("cart/emptyCart", async functio
 
 export const UpdateCartQty = createAsyncThunk("cart/updateQtyCart", async function ({id, quantity},  {rejectWithValue, dispatch}) {
   try {
-    const data = await commerce.cart.update(id, quantity)
+    const data = await commerce.cart.update(id, {'quantity': quantity})
     if (!data.success) {
       throw new Error('Can\'t add product!')
     }
